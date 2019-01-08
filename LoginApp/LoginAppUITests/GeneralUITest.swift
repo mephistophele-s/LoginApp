7ae43b2531af3d73db73ab2e2dde8957920744b4
Anastasia Starchencko <mephistopheles@macpaw.com>
2019-01-08 15:56:50 +0200

    Add UI tests.

---
+++ LoginApp/LoginAppUITests/GeneralUITest.swift
@@ -0,0 +1,22 @@
+//
+//  GeneralUITest.swift
+//  LoginApp
+//
+//  Created by Anastasia on 1/7/19.
+//  Copyright © 2019 Anastasia. All rights reserved.
+//
+
+import XCTest
+
+class BasicTestCase: XCTestCase {
+    
+    let app = XCUIApplication()
+    
+    override func setUp() {
+        super.setUp()
+    }
+    
+    override func tearDown() {
+        super.tearDown()
+    }
+}
