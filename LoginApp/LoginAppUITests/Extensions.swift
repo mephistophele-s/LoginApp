8ffbcd6985391a26646d752085c5551c7d8a1d62
Anastasia Starchencko <mephistopheles@macpaw.com>
2019-01-08 16:58:34 +0200

    Add UI tests.

---
+++ LoginApp/LoginAppUITests/Extensions.swift
@@ -0,0 +1,21 @@
+//
+//  Extensions.swift
+//  LoginAppUITests
+//
+//  Created by Yaroslav2 Kopylov2 on 1/7/19.
+//  Copyright © 2019 Anastasia. All rights reserved.
+//
+
+import Foundation
+import XCTest
+
+extension Array where Element: XCUIElement {
+    
+    func failedElements() -> [Element] {
+        return self.filter { $0.exists == false }
+    }
+}
+
+func AssertArrayIsEmpty(_ array: [XCUIElement]) {
+    XCTAssertTrue(array.isEmpty, "\(array.map { $0.description }) elements are not present")
+}
